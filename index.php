467ab38f0cc2c04637d5603091158871e974a38e
Michael Halat <michael.halat@inventivhealth.com>
2019-01-03 15:33:07 -0500

    added base files

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>J0hnny 5 Command Center</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+  <link rel="stylesheet" href="assets/reset.css?v=1.0">
+  <link rel="stylesheet" href="assets/styles.css?v=1.0">
+  <link rel="stylesheet" href="assets/debug.css">
+</head>
+
+<body>
+  <div class="container">
+    <div class="btn">Spot Lights 1</div>
+    <div class="btn">Spot Lights 2</div>
+    <div class="btn">Spot Lights 3</div>
+    <div class="btn">Spot Lights 4</div>
+    <div class="btn">Spot Lights 5</div>
+    <div class="btn">Spot Lights 6</div>
+    <div class="btn">Spot Lights 7</div>
+    <div class="btn">Spot Lights 8</div>
+  </div>
+</body>
+</html>
