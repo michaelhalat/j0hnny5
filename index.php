736cf114b981aafc5dd5828ce312de5581caa2fe
Michael Halat <michael.halat@inventivhealth.com>
2019-01-04 11:32:47 -0500

    modified sizes of hex's

--- index.php
+++ index.php
@@ -7,20 +7,26 @@
   <title>J0hnny 5 Command Center</title>
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
-  <link rel="stylesheet" href="assets/reset.css?v=1.0">
-  <link rel="stylesheet" href="assets/styles.css?v=1.0">
+  <link rel="stylesheet" href="assets/reset.css">
+  <link rel="stylesheet" href="assets/styles.css">
 </head>
 
 <body>
-  <div class="container">
-    <div class="btn">Spot Lights 1</div>
-    <div class="btn">Spot Lights 2</div>
-    <div class="btn">Spot Lights 3</div>
-    <div class="btn">Spot Lights 4</div>
-    <div class="btn">Spot Lights 5</div>
-    <div class="btn">Spot Lights 6</div>
-    <div class="btn">Spot Lights 7</div>
-    <div class="btn">Spot Lights 8</div>
+  <div class="viewport-container">
+    <div class="container">
+      <div class="item"><div class="hexagon"></div></div>
+      <div class="item"><div class="hexagon"></div></div>
+      <div class="item"><div class="hexagon"></div></div>
+      <div class="item"><div class="hexagon"></div></div>
+      <div class="item"><div class="hexagon"></div></div>
+      <div class="item"><div class="hexagon"></div></div>
+      <div class="item"><div class="hexagon"></div></div>
+      <div class="item"><div class="hexagon"></div></div>
+      <div class="item"><div class="hexagon"></div></div>
+      <div class="item"><div class="hexagon"></div></div>
+      <div class="item"><div class="hexagon"></div></div>
+    </div>
   </div>
+  
 </body>
 </html>
