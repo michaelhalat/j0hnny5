3a441e10373c02f8e5a95a026e1467fa7fd99ced
Halat <mhalat@mslofvfxp0u6.tuc.com>
2019-02-05 07:13:10 -0500

    slowly integrating php

--- index.php
+++ index.php
@@ -14,6 +14,28 @@
 </head>
 
 <body>
+  <?php
+  $val_array = array (0,0,0,0,0,0,0,0);
+  $pin = array (8,9,7,0,2,3,12,13);
+  //this php script generate the first page in function of the file
+  for ( $i= 0; $i<8; $i++) {
+    //set the pin's mode to output and read them
+    system("gpio mode ".$pin[$i]." out");
+    exec ("gpio read ".$pin[$i], $val_array[$i], $return );
+  }
+  //for loop to read the value
+	$i =0;
+	for ($i = 0; $i < 8; $i++) {
+		//if off
+		if ($val_array[$i][0] == 0 ) {
+			echo ("<img id='button_".$i."' src='data/img/red/red_".$i.".jpg' onclick='change_pin (".$i.");'/>");
+		}
+		//if on
+		if ($val_array[$i][0] == 1 ) {
+			echo ("<img id='button_".$i."' src='data/img/green/green_".$i.".jpg' onclick='change_pin (".$i.");'/>");
+		}	 
+	}
+  ?>
   <div class="viewport-container">
     <div class="container">
       <div class="item">
